f5423c91e64adb6ccb10a996e61c8df983d6274a
Georgij Zacharov <gosha.za@gmail.com>
2021-03-07 00:37:52 +0100

    Handle queries not found

--- src/set_title.js
+++ src/set_title.js
@@ -1,11 +1,11 @@
 let reviewCount = +(
-  document.querySelector(".lessons-and-reviews__reviews-button span").innerText ??
-  document.getElementById("review-queue-count").innerText ??
+  document.querySelector(".lessons-and-reviews__reviews-button span")?.innerText ??
+  document.getElementById("review-queue-count")?.innerText ??
   0
 )
 let lessonCount = +(
-  document.querySelector(".lessons-and-reviews__lessons-button span").innerText ??
-  document.getElementById("lesson-queue-count").innerText ??
+  document.querySelector(".lessons-and-reviews__lessons-button span")?.innerText ??
+  document.getElementById("lesson-queue-count")?.innerText ??
   0
 )
 
